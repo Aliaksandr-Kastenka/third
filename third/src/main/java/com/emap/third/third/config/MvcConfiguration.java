24bca699ae94bc3247be8eb62e2664f577ffcd14
Aliaksandr Kastenka <Aliaksandr_Kastenka@epam.com>
2014-05-07 15:22:47 +0300

    Another commit

--- third/src/main/java/com/emap/third/third/config/MvcConfiguration.java
+++ third/src/main/java/com/emap/third/third/config/MvcConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @EnableWebMvc
 public class MvcConfiguration extends WebMvcConfigurerAdapter{
 
+	//change something
 	@Bean
 	public ViewResolver getViewResolver(){
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
